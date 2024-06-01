f2b6eb07c8eab1685c12513b27d549b573a6c197
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-06-01 14:23:15 -0300

    Adding comments

--- list-studentsWithPhones.php
+++ list-studentsWithPhones.php
@@ -8,6 +8,7 @@ require_once 'vendor/autoload.php';
 $pdo = ConnectionCreator::Connection();
 $repository = new PdoStudentRepository($pdo);
 
+/** @var Student[] $studentList */
 $studentList = $repository->studentsWithPhones();
 
 var_dump($studentList);
